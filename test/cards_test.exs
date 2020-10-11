fb149bc002ef71e398288ffc166c0ec30ad196a2
Felipe Orlando <fobsouza@gmail.com>
2020-10-11 03:30:23 -0300

    separate tests on describes

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,26 +2,41 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "create_deck/1 makes 52 cards" do
-    deck_length = length(Cards.create_deck)
-    assert deck_length == 52
+  describe "create_deck/1" do
+    test "makes 52 cards" do
+      deck_length = length(Cards.create_deck)
+      assert deck_length == 52
+    end
   end
 
-  test "shuffle/2 gives a deck randomized" do
-    deck = Cards.create_deck()
-    refute deck == Cards.shuffle(deck)
+  describe "shuffle/2" do
+    test "gives a deck randomized" do
+      deck = Cards.create_deck()
+      refute deck == Cards.shuffle(deck)
+    end
   end
 
-  test "contains?/2 returns if a given deck has a specified card" do
-    deck = Cards.create_deck()
-    assert Cards.contains?(deck, "Ace of Spades") == true
+  describe "contains?/2" do
+    test "returns if a given deck has a specified card" do
+      deck = Cards.create_deck()
+      assert Cards.contains?(deck, "Ace of Spades") == true
+    end
   end
 
-  test "deal/2 returns a hand of a specified number of cards and the rest of the provided deck" do
-    deck = Cards.create_deck()
-    {first_card, rest_of_deck} = Cards.deal(deck, 1)
+  describe "deal/2" do
+    test "returns a hand of a specified number of cards" do
+      deck = Cards.create_deck()
+      {first_card, _} = Cards.deal(deck, 1)
 
-    assert first_card == ["Ace of Hearts"]
-    assert length(rest_of_deck) == 51
+      assert first_card == ["Ace of Hearts"]
+    end
+
+    test "returns the rest of the provided deck" do
+      deck = Cards.create_deck()
+      {_, rest_of_deck} = Cards.deal(deck, 1)
+
+      assert length(rest_of_deck) == 51
+    end
   end
+
 end
